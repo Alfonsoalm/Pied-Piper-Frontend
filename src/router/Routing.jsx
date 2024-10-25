5f6b02ef24320e5919d3609ce7044239c296213c
Alfonsoalm <alfonsoalm34@gmail.com>
2024-10-25 12:36:58 +0200

    Errores de importaciones solucionados en Routing

--- src/router/Routing.jsx
+++ src/router/Routing.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import { Routes, Route, BrowserRouter, Navigate, Link } from "react-router-dom";
-import { PublicLayout } from "../components/layout/public/PublicLayout";
-import { PrivateLayout } from "../components/layout/private/PrivateLayout";
-import { Login } from "../components/user/Login";
-import { Register } from "../components/user/Register";
-import { Feed } from "../components/publication/feed";
+import { PublicLayout } from "../components/layout/public/PublicLayout.jsx";
+import { PrivateLayout } from "../components/layout/private/PrivateLayout.jsx";
+import { Login } from "../components/user/Login.jsx";
+import { Register } from "../components/user/Register.jsx";
+import { Feed } from "../components/publication/Feed.jsx";
 import { AuthProvider } from "../context/AuthProvider";
-import { Logout } from "../components/user/Logout";
-import { People } from "../components/user/People";
-import { Config } from "../components/user/Config";
-import { Following } from "../components/follow/Following";
-import { Followers } from "../components/follow/followers";
-import { Profile } from "../components/user/Profile";
+import { Logout } from "../components/user/Logout.jsx";
+import { People } from "../components/user/People.jsx";
+import { Config } from "../components/user/Config.jsx";
+import { Following } from "../components/follow/Following.jsx";
+import { Followers } from "../components/follow/Followers.jsx";
+import { Profile } from "../components/user/Profile.jsx";
 
 export const Routing = () => {
   return (
